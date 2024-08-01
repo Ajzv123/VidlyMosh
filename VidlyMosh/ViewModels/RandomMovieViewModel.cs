bbc6fdfddc2b38ea8a39bed2a6526eda856373e6
Ajzv123 <95384676+Ajzv123@users.noreply.github.com>
2024-08-01 16:49:46 -0600

    Agregar archivos de proyecto.

---
+++ VidlyMosh/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyMosh.Models;
+using VidlyMosh.Models;
+
+namespace VidlyMosh.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
